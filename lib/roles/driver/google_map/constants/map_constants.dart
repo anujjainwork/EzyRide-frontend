10a4b69046ee71bbeed1d6f8e03f1054211e5466
anujjainwork <jainanuj.work@gmail.com>
2024-12-28 15:07:02 +0530

    constant-change

--- lib/roles/driver/google_map/constants/map_constants.dart
+++ lib/roles/driver/google_map/constants/map_constants.dart
@@ -1,4 +1,3 @@
 class Constants {
-  static const String googleApiKey = 'AIzaSyAy9k4eOlYF0UdULFWbtafU3eG-G3puM9E';
-  // String.fromEnvironment('GOOGLE_API_KEY');
+  static const String googleApiKey = String.fromEnvironment('GOOGLE_API_KEY');
 }
